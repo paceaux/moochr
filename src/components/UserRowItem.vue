48af66b772ffb044dbc29f0f3d21519a058c59c0
Frank Taylor <frank@tahzoo.com>
2018-03-27 22:16:10 -0500

    adds input types and imroves readability

--- src/components/UserRowItem.vue
+++ src/components/UserRowItem.vue
@@ -1,17 +1,36 @@
 <template>
     <tr class="userTable__userRow">
         <td class="userTable__cell" headers="id id">{{user.id}}</td>
-        <td class="userTable__cell" headers="firstname name"><input :disabled="!isEditable" v-model="user.firstname"/></td>
-        <td class="userTable__cell" headers="lastname name"><input :disabled="!isEditable" v-model="user.lastname"/>
-            </td>
-        <td class="userTable__cell" headers="email contact"><input :disabled="!isEditable" v-model="user.email"/></td>
-        <td class="userTable__cell" headers="phone contact"><input :disabled="!isEditable" v-model="user.phone"/></td>
-        <td class="userTable__cell" headers="street1 address"><input :disabled="!isEditable" v-model="user.street1"/></td>
-        <td class="userTable__cell" headers="street2 address"><input :disabled="!isEditable" v-model="user.street2"/></td>
-        <td class="userTable__cell" headers="zip address"><input :disabled="!isEditable" v-model="user.zip"/></td>
-        <td class="userTable__cell" headers="city address"><input :disabled="!isEditable" v-model="user.city"/></td>
-        <td class="userTable__cell" headers="state address"><input :disabled="!isEditable" v-model="user.state"/></td>
-        <td class="userTable__cell" headers="country address"><input :disabled="!isEditable" v-model="user.country"/></td>
+        <td class="userTable__cell" headers="firstname name">
+            <input :disabled="!isEditable" v-model="user.firstname" type="text"/>
+        </td>
+        <td class="userTable__cell" headers="lastname name">
+            <input :disabled="!isEditable" v-model="user.lastname" type="text" />
+        </td>
+        <td class="userTable__cell" headers="email contact">
+            <input :disabled="!isEditable" v-model="user.email" type="email"/>
+        </td>
+        <td class="userTable__cell" headers="phone contact">
+            <input :disabled="!isEditable" v-model="user.phone" type="tel"/>
+        </td>
+        <td class="userTable__cell" headers="street1 address">
+            <input :disabled="!isEditable" v-model="user.street1" type="text"/>
+        </td>
+        <td class="userTable__cell" headers="street2 address">
+            <input :disabled="!isEditable" v-model="user.street2" type="text"/>
+        </td>
+        <td class="userTable__cell" headers="zip address">
+            <input :disabled="!isEditable" v-model="user.zip" type="text"/>
+        </td>
+        <td class="userTable__cell" headers="city address">
+            <input :disabled="!isEditable" v-model="user.city" type="text"/>
+        </td>
+        <td class="userTable__cell" headers="state address">
+            <input :disabled="!isEditable" v-model="user.state" type="text"/>
+        </td>
+        <td class="userTable__cell" headers="country address">
+            <input :disabled="!isEditable" v-model="user.country" type="text"/>
+        </td>
         <td class="userTable__cell">
             <button v-on:click="toggleEdit">edit</button>
             <button v-show="isEditable" @click="updateContent()">save</button>
