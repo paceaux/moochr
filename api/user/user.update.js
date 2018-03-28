9dc7b6defa251291cea4aff198481f2b6e7493f4
Frank Taylor <frank@tahzoo.com>
2018-03-28 08:50:16 -0500

    makes user update flexible if new cols get added later

--- api/user/user.update.js
+++ api/user/user.update.js
@@ -6,11 +6,12 @@ module.exports = function updateCb(req, res, done) {
     const client = new Client(dbConfig);
     const data = req.body;
     const id = req.params.user_id;
-    const sqlData = [data.firstname, data.lastname, data.email, data.street1,data.street2, data.city, data.state, data.country, id];
+    const dataKeys = Object.keys(data).filter(key=> key != 'id' || key != 'timestamp');
+    const setArg = dataKeys.map(key => `${key}='${data[key]}'`).toString();
 
     client.connect()
     .then(()=>{
-        client.query('UPDATE users SET firstname=($1), lastname=($2), email=($3),street1=($4),street2=($5),city=($6), state=($7), country=($8)  WHERE id=($9)',sqlData)
+        client.query(`UPDATE users SET ${setArg}  WHERE id=${id}`)
         .then(() => {
             client.query('SELECT * FROM users ORDER BY id ASC')
             .then((rowRes)=> {
