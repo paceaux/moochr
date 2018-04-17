99aac997f3d8fc9d183cea77923d24f06d8fd025
Frank Taylor <frank@tahzoo.com>
2018-04-17 14:22:03 -0500

    adds ability to get one category

--- api/category/index.js
+++ api/category/index.js
@@ -7,12 +7,13 @@ const categoryApi = {
   delete: require('./category.delete.js')
 };
 router.get('/', function(req, res, next) {
-  
+
   res.header('Content-Type', 'text/html').sendFile('public/index.html');
 });
 
 router.post('/api/v1/category', categoryApi.create );
 router.get('/api/v1/category', categoryApi.read);
+router.get('/api/v1/category/:category_id', categoryApi.read);
 router.put('/api/v1/category/:category_id', categoryApi.update);
 router.delete('/api/v1/category/:category_id', categoryApi.delete);
 
