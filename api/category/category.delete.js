a57be38311cc5415b260109699be8252489b3b6f
Frank Taylor <frank@tahzoo.com>
2018-04-18 17:51:33 -0500

    removes comments

--- api/category/category.delete.js
+++ api/category/category.delete.js
@@ -30,26 +30,4 @@ module.exports = function deleteCb(req,res, done) {
             console.log(err);
             res.status(500).send({error: err, crudOp: 'connection'});
         });
-    // const client = new Client(dbConfig);
-    // const id = req.params.category_id;
-
-    // client.connect()
-    // .then(() => {
-    //     client.query('DELETE FROM categories WHERE id=($1)', [id])
-    //     .then(()=>{
-    //         client.query('DELETE FROM categories WHERE id=($1)', [id])
-    //         .then((rowRes) => {
-    //             res.send(rowRes.rows);
-    //             client.end();
-    //         });
-    //     })
-    //     .catch(queryErr => {
-    //         res.status(500).send({error: queryErr, crudOp});
-    //         client.end();
-    //     });
-    // })
-    // .catch(err => {
-    //     res.status(500).send({error:err, crudOp: 'connection'});
-    //     client.end();
-    // });
 };
