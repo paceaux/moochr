790029f650a4ffa39f824d6b33f5f6ed5e39bb9f
Frank Taylor <frank@tahzoo.com>
2018-04-17 21:05:59 -0500

    starts on a sequelize

--- api/category/category.read.js
+++ api/category/category.read.js
@@ -1,27 +1,72 @@
 const dbConfig = require('../../db.config');
 const {Client, Query} = require('pg');
 const crudOp = 'read';
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+    dialect: 'postgres',
+    host: dbConfig.host,
+    port: dbConfig.port
+});
+
 
 module.exports = function readCb(req, res, next) {
-    const client = new Client(dbConfig);
+    // const client = new Client(dbConfig);
     const catId = req.params.category_id;
     const query = catId ? `SELECT * FROM categories WHERE id=${catId}` : 'SELECT * FROM categories ORDER BY id ASC';
 
-        client.connect()
-        .then(()=>{
-            client.query(query)
-            .then(queryRes=> {
-                const result = catId ? queryRes.rows[0] : queryRes.rows;
-                res.send(result);
-                client.end();
-            })
-            .catch(queryErr=>{
-                res.status(500).send({error: queryErr,crudOp});
-                client.end();
+    const Category = sequelize.define('category', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+
+        },
+        name: {
+            type: Sequelize.STRING
+        },
+        slug: {
+            type: Sequelize.STRING
+        },
+        parent: {
+            type: Sequelize.INTEGER
+        },
+        timestamp: Sequelize.DATE
+    });
+
+    Category.sync()
+        .then(() => {
+            return Category.create({
+                name: 'sync-test',
+                slug: 'whatevs'
             });
-        })
-        .catch(err => {
-            res.status(500).send({error:err,crudOp: 'connection'});
-            client.end();
         });
+
+        sequelize
+        .authenticate()
+        .then( ()=> {
+            console.log('yay!');
+            Category.findAll().then((users) => {
+                res.send(users);
+            })
+        })
+        .catch(err=> {
+            console.error('bad connect');
+        });
+        // client.connect()
+        // .then(()=>{
+        //     client.query(query)
+        //     .then(queryRes=> {
+        //         const result = catId ? queryRes.rows[0] : queryRes.rows;
+        //         res.send(result);
+        //         client.end();
+        //     })
+        //     .catch(queryErr=>{
+        //         res.status(500).send({error: queryErr,crudOp});
+        //         client.end();
+        //     });
+        // })
+        // .catch(err => {
+        //     res.status(500).send({error:err,crudOp: 'connection'});
+        //     client.end();
+        // });
     };
