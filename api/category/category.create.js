30b75550eeeee51a551f33e7451a7ecd2f5313dd
Frank Taylor <frank@tahzoo.com>
2018-04-18 17:12:04 -0500

    adds create op w/ sequelize

--- api/category/category.create.js
+++ api/category/category.create.js
@@ -1,32 +1,47 @@
-const dbConfig = require('../../db.config');
-const {Client} = require('pg');
 const crudOp = 'create';
+const Sequelize = require('sequelize');
+const sequelize = require('../sql.config');
+const Category = require('../../models/category.model');
+
 
 module.exports = function postCb (req, res, next) {
-    const client = new Client(dbConfig);
 
-    const data = req.body;
-    const timestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    data.timestamp = timestamp;
-    const dataKeys = Object.keys(data).toString();
-    const sqlData = Object.keys(data).map( key=>  typeof data[key] == 'string' ? `'${data[key]}'` : data[key]);
-  
-    const query = `INSERT INTO categories(${dataKeys}) values(${sqlData.toString()}) RETURNING *`;
-    // Get a Postgres client from the connection pool
-    client.connect()
-    .then(()=>{
-      client.query(query)
-      .then((queryRes) => {
-        res.send(queryRes.rows[0]);
+    sequelize
+      .authenticate()
+      .then(  () => {
+        return Category
+          .create(req.body)
+          .then(category => {
+            res.status(200).send(category.dataValues);
+          });
       })
-      .catch(queryErr => {
-        console.log(queryErr);
-        res.status(500).send({error: queryErr, crudOp});
-        client.end();
+      .catch(err => {
+        res.status(500).send({error: err, crudOp});
       });
-    })
-    .catch((err)=>{
-      res.status(500).send({error: err, crudOp: 'connection'});
-      client.end();
-    });
+    // const client = new Client(dbConfig);
+
+    // const data = req.body;
+    // const timestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    // data.timestamp = timestamp;
+    // const dataKeys = Object.keys(data).toString();
+    // const sqlData = Object.keys(data).map( key=>  typeof data[key] == 'string' ? `'${data[key]}'` : data[key]);
+  
+    // const query = `INSERT INTO categories(${dataKeys}) values(${sqlData.toString()}) RETURNING *`;
+    // // Get a Postgres client from the connection pool
+    // client.connect()
+    // .then(()=>{
+    //   client.query(query)
+    //   .then((queryRes) => {
+    //     res.send(queryRes.rows[0]);
+    //   })
+    //   .catch(queryErr => {
+    //     console.log(queryErr);
+    //     res.status(500).send({error: queryErr, crudOp});
+    //     client.end();
+    //   });
+    // })
+    // .catch((err)=>{
+    //   res.status(500).send({error: err, crudOp: 'connection'});
+    //   client.end();
+    // });
 };
