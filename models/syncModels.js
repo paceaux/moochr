d363080f7813a173553d7d1d3499075dffd9518c
Frank Taylor <frank@tahzoo.com>
2018-04-18 21:23:54 -0500

    adds user model

--- models/syncModels.js
+++ models/syncModels.js
@@ -2,7 +2,12 @@ const Sequelize = require('sequelize');
 const sequelize = require('../api/sql.config');
 
 const Category = require('./category.model');
+const User = require('./user.model');
 
 Category
     .sync()
-    .then(() => console.log('syncrhonized'));
+    .then(() => console.log('Category Synchronized'));
+
+User
+    .sync()
+    .then( () => console.log('User Synchronized'));
---
+++ models/user.model.js
@@ -0,0 +1,72 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../api/sql.config');
+
+const User = sequelize.define('user', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        firstname: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+        },
+        lastname: {
+            type: Sequelize.TEXT,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+            set(val) {
+                this.setDataValue('email', val.toLowerCase().trim());
+            }
+        },
+        password: {
+            type: Sequelize.VIRTUAL,
+            allowNull: true
+        },
+        phone: {
+            type: Sequelize.TEXT,
+            allowNull: true,
+            set (val) {
+                this.setDataValue('phone', val.replace(/\s/g, ''));
+            }
+        },
+        street1: {
+            type: Sequelize.TEXT,
+            allowNull: true
+        },
+        street2: {
+            type: Sequelize.TEXT,
+            allowNull: true
+        },
+        zip: {
+            type: Sequelize.TEXT,
+            allowNull: true,
+            set(val) {
+                this.setDataValue('zip', val.trim());
+            }
+        },
+        city: {
+            type: Sequelize.TEXT,
+            allowNull: true
+        },
+        state: {
+            type: Sequelize.TEXT,
+            allowNull: true
+        },
+        country: {
+            type: Sequelize.TEXT,
+            allowNull: true
+        }
+    },
+    {
+        tableName: 'users',
+        underscored: true,
+        updatedAt: false,
+        createdAt: 'timestamp'
+    }
+);
+
+module.exports = User;
