a55b908afd4cb00599c6a923592d952841de3848
Frank Taylor <frank@tahzoo.com>
2018-05-02 14:28:20 -0500

    Creates a userCard for getting information about loaning

---
+++ src/components/UserCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <article class="card card--user">
+      <h2>{{name}}</h2>
+      <div v-if="!hideContact" class="card__contact">
+          <a v-if="user.email" class="card__email" :href="emailLink">{{user.email}}</a>
+          <a v-if="user.phone" class="card__phone" :href="telLink">{{user.phone}}</a>
+      </div>
+      <div v-if="!hideAddress" class="card__address">
+          <p v-if="user.street1 && !hideStreet" class="card__street">{{user.street1}}</p>
+          <p v-if="user.street2 && !hideStreet" class="card__street">{{user.street2}}</p>
+          <p class="card__region">
+            <span v-if="user.city">{{user.city}}</span>
+            <span v-if="user.state">{{user.state}}</span>
+            <span v-if="user.zip">{{user.zip}}</span>
+          </p>
+          <p v-if="user.country" class="card__country">{{user.country}}</p>
+      </div>
+  </article>
+</template>
+<script>
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        },
+        hideAddress: {
+            type: Boolean,
+            required: false
+        },
+        hideContact: {
+            type: Boolean,
+            required: false
+        },
+        hideStreet: {
+            type: Boolean,
+            required: false
+        }
+    },
+    data() {
+        return {};
+    },
+    computed: {
+        name() {
+            const first = this.user.firstname ? this.user.firstname : '';
+            const last = this.user.lastname ? this.user.lastname : '';
+
+            return `${first} ${last}`;
+        },
+        emailLink() {
+            return `mailto:${this.user.email}`;
+        },
+        telLink() {
+            return `tel:${this.user.phone}`;
+        }
+    }
+}
+</script>
