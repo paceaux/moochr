ab1be5983367cbd99526f3dfb8ae10276e4056c5
Frank Taylor <frank@tahzoo.com>
2018-05-15 12:45:02 -0500

    adds item cards

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <article class="card card--item" v-if="item">
+      <figure class="card__media">
+          <img v-if="item.image" :src="item.image" />
+      </figure>
+        <div class="card__info">
+            <h2 v-if="item.name" class="card__name">{{item.name}}</h2>
+            <p v-if="item.model_number" class="card__model">Model: {{item.model_number}}</p>
+            <p v-if="item.serial_number" class="card__serial">Serial: {{item.serial_number}}</p>
+            <p v-if="item.value && !hideValue" class="card__value">Value: {{item.value}}</p>
+        </div>
+        <div class="card__owner" v-if="item.owner && !hideOwner">
+            <h3 class="card__sectionTitle">Owned By:</h3>
+            <UserCard class="card__owner" :user="itemOwner" :hideAddress="true"></UserCard>
+        </div>
+        <div class="card__borrower" v-if="item.borrower && !hideBorrower">
+            <h3 class="card__sectionTitle">Borrowed By: </h3>
+            <UserCard class="card__borrower" :user="itemBorrower" :hideAddress="true"></UserCard>
+            <p v-if="item.time_loaned">Loaned on:<time datetime="item.time_loaned">{{loanedOn}}</time></p>
+            <p v-if="item.time_return">due on:<time datetime="item.time_return">{{dueOn}}</time></p>
+        </div>
+  </article>
+</template>
+<script>
+import TimeHelper from '../helpers/timehelper';
+import UserCard from './UserCard.vue';
+export default {
+    components: {
+        UserCard
+    },
+    props: {
+        item: {
+            type: Object,
+            required: false
+        },
+        hideOwner: {
+            type: Boolean,
+            required: false
+        },
+        hideCategory: {
+            type: Boolean,
+            required: false,
+        },
+        hideBorrower: {
+            type: Boolean,
+            required: false
+        },
+        hideValue: {
+            type: Boolean,
+            required: false
+        }
+
+    },
+    data () {
+        return {}
+    },
+    computed: {
+        itemOwner() {
+            return this.$store.getters.userById(this.owner);
+        },
+        itemBorrower() {
+            return this.$store.getters.userById(this.item.borrower);
+        },
+        categoryName() {
+        },
+        loanedOn() {
+            return TimeHelper.getTextDate(this.item.time_loaned);
+        },
+        dueOn() {
+            return TimeHelper.getTextDate(this.item.time_due);
+        }
+    },
+    beforeCreate() {
+        console.log(this);
+    }
+}
+</script>
---
+++ src/components/ItemCards.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="cardDeck">
+        <h2>Items</h2>
+        <ItemCard v-for="item in items" :key="item.id" :item="item" :hideOwner="hideOwner"></ItemCard>
+    </section>
+</template>
+<script>
+import ItemCard from './ItemCard.vue';
+
+export default {
+    components: {
+        ItemCard
+    },
+    props: {
+        filteredItems: {
+            type: Array,
+            required: false
+        },
+        hideOwner: {
+            type: Boolean,
+            required: false
+        },
+        hideCategory: {
+            type: Boolean,
+            required: false,
+        },
+        hideBorrower: {
+            type: Boolean,
+            required: false
+        },
+        hideValue: {
+            type: Boolean,
+            required: false
+        }
+    },
+    data() {
+        return {
+            allItems: this.$store.state.items
+        };
+    },
+    computed: {
+        items() {
+            return this.filteredItems || this.allItems;
+        }
+    },
+    mounted() {
+        console.log(this.items);
+    }
+}
+</script>
