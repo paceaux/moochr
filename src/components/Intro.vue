f1fc6a64aaef11284ef408e1a67c77aea097fb7e
Frank Taylor <frank@tahzoo.com>
2018-05-16 13:31:07 -0500

    cleans up top nav, makes admin links collapsible

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,20 +1,31 @@
 <template>
-  <header>
-      <nav>
-        <router-link to="/addUser">Add User</router-link>
-        <router-link to="/userList">User Table</router-link>
-        <router-link to="/addCategory">Add a Category</router-link>
-        <router-link to="/categoryList">Categories</router-link>
-        <router-link to="/addItem">Add an Item</router-link>        
-        <router-link to="/itemList">List of Items</router-link>
+  <header class="appHeader">
+      <nav class="appHeader__adminNav nav nav--vertical">
+        <h2 class="nav__title" @click="isNavVisible = !isNavVisible">Admin</h2>
+        <div class="nav__links" v-show="isNavVisible">
+          <router-link class="nav__link" to="/admin/addUser">Add User</router-link>
+          <router-link class="nav__link" to="/admin/userList">User Table</router-link>
+          <router-link class="nav__link" to="/admin/addCategory">Add a Category</router-link>
+          <router-link class="nav__link" to="/admin/categoryList">Categories</router-link>
+          <router-link class="nav__link" to="/admin/addItem">Add an Item</router-link>        
+          <router-link class="nav__link" to="/admin/itemList">List of Items</router-link>
+        </div>
+      </nav>
+      <nav class="appHeader__mainNav nav nav--horizontal">
+        <router-link class="nav__link" to="/users">Users</router-link>
+        <router-link class="nav__link" to="/items">Items</router-link>
       </nav>
   </header>
 </template>
-
 <script>
 export default {
     props: {
 
+    },
+    data() {
+      return {
+        isNavVisible: false
+      };
     },
     computed: {
 
