12b82d27996708ef1297813339abef644a673182
Frank Taylor <frank@tahzoo.com>
2018-05-16 09:36:52 -0500

    uses more generic names of sections

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -9,11 +9,11 @@
             <p v-if="item.serial_number" class="card__text">Serial: {{item.serial_number}}</p>
             <p v-if="item.value && !hideValue" class="card__text">Value: {{item.value}}</p>
         </div>
-        <div class="card__owner" v-if="item.owner && !hideOwner">
+        <div class="card__section" v-if="item.owner && !hideOwner">
             <h3 class="card__sectionTitle">Owned By:</h3>
             <UserCard class="card__owner" :user="itemOwner" :hideAddress="true"></UserCard>
         </div>
-        <div class="card__borrower" v-if="item.borrower && !hideBorrower">
+        <div class="card__section" v-if="item.borrower && !hideBorrower">
             <h3 class="card__sectionTitle">Borrowed By: </h3>
             <UserCard class="card__borrower" :user="itemBorrower" :hideAddress="true"></UserCard>
             <p class="card__text" v-if="item.time_loaned">Loaned on:<time datetime="item.time_loaned">{{loanedOn}}</time></p>
@@ -56,7 +56,7 @@ export default {
     },
     computed: {
         itemOwner() {
-            return this.$store.getters.userById(this.owner);
+            return this.$store.getters.userById(this.item.owner);
         },
         itemBorrower() {
             return this.$store.getters.userById(this.item.borrower);
