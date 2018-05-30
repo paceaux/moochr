9cf9579677586e7993162662197e9c8d3f23aad8
Frank Taylor <frank@tahzoo.com>
2018-05-30 14:43:04 -0500

    Puts app on Koa

--- app.js
+++ app.js
@@ -1,43 +1,30 @@
-const express = require('express');
-const app = express();
+const Koa = require('koa');
+const Serve = require('koa-static');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const router = new Router();
+const app = new Koa();
 const path = require('path');
-const favicon = require('serve-favicon');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 
-const users = require('./api/user/index');
-const categoryCrud = require('./api/category/index');
-const itemCrud = require('./api/item/index');
+const categoryCrud = require('./api/category/index.js');
+const userCrud = require('./api/user/index.js');
+const itemCrud = require('./api/item/index.js');
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', users);
-app.use('/', categoryCrud);
-app.use('/', itemCrud);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-  // todo: forward a 404 page
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send('error'); // todo: forward a 500 page
-});
+app
+    .use(bodyParser({
+        enableTypes: ['json', 'form'],
+    }))
+    .use(Serve(path.join(__dirname, 'public')))
+    .use(router.routes())
+    .use(categoryCrud.routes())
+    .use(userCrud.routes())
+    .use(itemCrud.routes())
+    .use(router.allowedMethods());
 
 module.exports = app;
+
+/*  todo:
+  add error handling.
+  consider putting API on diff port
+*/
