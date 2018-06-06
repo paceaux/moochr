7022501bdad7985de8229a7034d17ce9c99dc4fc
Frank Taylor <frank@tahzoo.com>
2018-06-06 13:31:53 -0500

    adds markup for login page and form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,45 @@
+<template>
+        <form class="form form--authenticate">
+            <h2 class="form__title">Sign in to Moochr</h2>
+            <fieldset class="form__fieldset form__fieldset--credentials">
+                <label class="form__field">
+                    <span class="form__fieldLabel">Email</span>
+                    <input
+                        class="form__fieldInput form__fieldInput--email"
+                        type="email"
+                        v-model="email"
+                        required
+                    />
+                    <output class="form__fieldInfo"></output>
+                </label>
+                <label class="form__field">
+                    <span class="form__fieldLabel">Password</span>
+                    <input
+                        class="form__fieldInput form__fieldInput--email"
+                        type="text"
+                        v-model="password"
+                        required
+                    />
+                    <output class="form__fieldInfo"></output>
+                </label>
+                <output class="form__fieldsetInfo"></output>
+            </fieldset>
+            <fieldset class="form__fieldset form__fieldset--controls">
+                <button class="form__submit">Log In</button>
+            </fieldset>
+        </form>
+</template>
+<script>
+export default {
+    components: {},
+    data() {
+        return {
+            email: '',
+            password: '',
+        };
+    },
+    props: {},
+    computed: {},
+    methods: {}
+}
+</script>
---
+++ src/components/LoginSection.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="frontdoor">
+        <LoginForm></LoginForm>
+    </section>
+</template>
+<script>
+import LoginForm from './Loginform.vue';
+
+export default {
+    components : {
+        LoginForm,
+    },
+    data() {
+        return {};
+    },
+    computed: {},
+    methods: {}
+
+}
+</script>
