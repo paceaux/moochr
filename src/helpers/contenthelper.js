55eb60ff5d3eedc21e6a631bb46e8e10d0b3bf3d
Frank Taylor <frank@tahzoo.com>
2018-06-06 15:06:33 -0500

    creates a central location for any content I might need

---
+++ src/helpers/contenthelper.js
@@ -0,0 +1,9 @@
+export default {
+    ErrorMessages: {
+        noEmail: 'Email is required',
+        invalidEmail: 'That is not a valid email address',
+        noPassword: 'Password is required',
+        invalidPassword: 'That isn\'t a valid password',
+        badAuth: 'Either the username or password is incorrect.',
+    },
+};
