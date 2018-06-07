4e876866c8d3aacd4dfddec454a2ef1e892a6004
Paceaux <frank.m.taylor08@gmail.com>
2018-06-07 09:11:09 -0500

    adds auth api to app

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const Serve = require('koa-static');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const session = require('koa-session');
+const passport = require('koa-passport');
 
 const router = new Router();
 const app = new Koa();
@@ -10,16 +12,23 @@ const path = require('path');
 const categoryCrud = require('./api/category/index.js');
 const userCrud = require('./api/user/index.js');
 const itemCrud = require('./api/item/index.js');
+const authCrud = require('./api/auth/index.js');
+
+app.keys = ['secret-key'];
+app.use(session(app));
 
 app
     .use(bodyParser({
         enableTypes: ['json', 'form'],
     }))
     .use(Serve(path.join(__dirname, 'public')))
+    .use(passport.initialize())
+    .use(passport.session())
     .use(router.routes())
     .use(categoryCrud.routes())
     .use(userCrud.routes())
     .use(itemCrud.routes())
+    .use(authCrud.routes())
     .use(router.allowedMethods());
 
 module.exports = app;
