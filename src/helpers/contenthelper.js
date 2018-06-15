04349c519879d8988de950af6d6614d8ebd3583e
Frank Taylor <frank@tahzoo.com>
2018-06-15 17:34:07 -0500

    adds message if user is missing

--- src/helpers/contenthelper.js
+++ src/helpers/contenthelper.js
@@ -2,6 +2,7 @@ export default {
     ErrorMessages: {
         noEmail: 'Email is required',
         invalidEmail: 'That is not a valid email address',
+        noUserWithEmail: 'We don\'t have that email in our system',
         noPassword: 'Password is required',
         invalidPassword: 'That isn\'t a valid password',
         badAuth: 'Either the username or password is incorrect.',
