a07d99e4f51c76434fd98851828e1bdc65fb86b7
Frank Taylor <frank@tahzoo.com>
2018-08-10 13:23:50 -0500

    changes database details

--- db.config.js
+++ db.config.js
@@ -1,26 +1,26 @@
 const Sequelize = require('sequelize');
 
 const dbConfig = {
-    user: 'todo',
+    user: 'moochrapp',
     host: 'localhost',
-    database: 'todo',
-    password: 'todo',
-    port: 5432
-  };
+    database: 'moochr',
+    password: '#(~q#>FDdJEU2}{',
+    port: 5432,
+};
 
-  module.exports = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
-      dialect: 'postgres',
-      host: dbConfig.host,
-      port: dbConfig.port,
-      logging: false
-  });
+module.exports = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+    dialect: 'postgres',
+    host: dbConfig.host,
+    port: dbConfig.port,
+    logging: false,
+});
 /* Create Database
-  Command:  CREATE DATABASE todo;
+  Command:  CREATE DATABASE moochr;
   Validate: \l
 */
 
 /* Create user:
-  Command:   CREATE ROLE todo WITH LOGIN PASSWORD 'todo';
+  Command:   CREATE ROLE moochrapp WITH LOGIN PASSWORD '#(~q#>FDdJEU2{';
   Validate: \du
 */
 
