0b7b6066d10bc9ff83e52a6b5d00ab67d5e78e8c
Frank Taylor <frank@tahzoo.com>
2018-08-14 16:53:42 -0500

    changes order in which models are synced. Also sets process to exit.

--- models/syncModels.js
+++ models/syncModels.js
@@ -6,19 +6,25 @@ const Item = require('./item.model');
 async function createModels() {
     await Category
         .sync({ force: true })
-        .then(() => console.log('Category Synchronized'));
-
-    await User
-        .sync({ force: true })
-        .then(() => console.log('User Data Synchronized'));
+        .then(() => console.log('Category Synchronized'))
+        .catch(err => console.log(err));
 
     await UserAuth
         .sync({ force: true })
-        .then(() => console.log('User Synchronized'));
+        .then(() => console.log('User Synchronized'))
+        .catch(err => console.log(err));
+
+    await User
+        .sync({ force: true })
+        .then(() => console.log('User Data Synchronized'))
+        .catch(err => console.log(err));
 
     await Item
         .sync({ force: true })
-        .then(() => console.log('Item Synchronized'));
-};
+        .then(() => console.log('Item Synchronized'))
+        .catch(err => console.log(err));
+
+    process.exit();
+}
 
 createModels();
