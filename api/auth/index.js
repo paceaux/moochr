0b8386022aea727d5fb8abfcf9e3f0c5c54ce47f
Frank Taylor <frank@tahzoo.com>
2018-08-16 20:10:03 -0500

    adds update user controls

--- api/auth/index.js
+++ api/auth/index.js
@@ -6,8 +6,10 @@ const router = new Router({
 
 const register = require('./register.js');
 const deleteuser = require('./user.delete.js');
+const update = require('./user.update.js');
 
 router.post('/register', register);
+router.put('/auth/user/:id', update.byId);
 router.delete('/auth/deleteuser', deleteuser);
 
 module.exports = router;
