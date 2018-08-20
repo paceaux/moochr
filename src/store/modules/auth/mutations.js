92f8c184afbaf876d5ed590db6bec3843d95271b
Frank Taylor <frank@tahzoo.com>
2018-08-20 10:33:34 -0500

    adds actions for explicitely managing token

--- src/store/modules/auth/mutations.js
+++ src/store/modules/auth/mutations.js
@@ -1,10 +1,15 @@
-import Vue from 'vue';
-
 export default {
     LOGIN(state, loginData) {
         state.isAuthenticated = true;
-        state.token = loginData.token;
         state.currentUser = loginData.email;
         state.authenticationId = loginData.id;
+    },
+    UPDATETOKEN(state, newToken) {
+        localStorage.setItem('MUT', newToken);
+        state.token = newToken;
+    },
+    REMOVETOKEN(state) {
+        localStorage.removeItem('MUT');
+        state.token = null;
     },
 };
